c80633f311af5f6678b8aaceb2e546ef0bca3b2c
DELL <DELL@DESKTOP-04S95NF>
2024-09-16 20:59:41 +0530

    Add project files.

---
+++ WingHerryTask/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WingHerryTask.Models
+{
+    public class User
+    {
+            [Key]
+            public int Id { get; set; }  
+
+            [Required(ErrorMessage = "Name is required.")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Email is required.")]
+            [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+            [RegularExpression(@"^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,6}$", ErrorMessage = "Invalid Email Address.")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Mobile No is required.")]
+            [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile No should be number and 10 digits only.")]
+            [MinLength(10, ErrorMessage = "Mobile No should be number and 10 digits only.")]
+            [MaxLength(10, ErrorMessage = "Mobile No should be number and 10 digits only.")]
+            public string MobileNo { get; set; }
+
+            [Required(ErrorMessage = "Address is required.")]
+            public string Address { get; set; }
+
+            [Required(ErrorMessage = "Gender is required.")]
+            public string Gender { get; set; }  // Will hold values like "Male" or "Female"
+        
+
+    }
+}
---
+++ WingHerryTask/Models/WingDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace WingHerryTask.Models
+{
+    public class WingDbContext:DbContext
+    {
+        public WingDbContext():base("Constr")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<WingDbContext>());
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
